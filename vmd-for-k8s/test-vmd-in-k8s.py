5fec566dde464123aa05897ca84f79cc32a350a5
lizhun <freelizhun@qq.com>
2020-12-03 00:14:27 +0800

    add test-vmd-in-k8s file

---
+++ vmd-for-k8s/test-vmd-in-k8s.py
@@ -0,0 +1,94 @@
+from kubernetes import client, config
+import time, re, os, sys
+import tarfile
+from tempfile import TemporaryFile
+
+from kubernetes.client import Configuration
+from kubernetes.client.api import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+
+#kubernetes 12.0.1
+def exec_commands(api_instance,pod_name,namespace,input_file_name):
+    name = 'ubuntu-desk-vmd-7989c5946d-fqsr8'
+    exec_namespace=namespace
+    resp = None
+    try:
+        resp = api_instance.read_namespaced_pod(name=name,
+                                                namespace=exec_namespace)
+    except ApiException as e:
+        if e.status != 404:
+            print("Unknown error: %s" % e)
+            exit(1)
+
+    if not resp:
+        print("Pod %s does not exist. Please creating it..." % name)
+        exit(1)
+
+    # Begining to cp source_file from galaxy-web-*** pod to ubuntu-desk-vmd-*** pod's dest_file
+    try:
+        exec_command_forcp = ["tar", "xvf", "-", "-C", "/"]
+        resp_forcp = stream(api_instance.connect_get_namespaced_pod_exec,
+                        name,
+                        exec_namespace,
+                        command=exec_command_forcp,
+                        stderr=True, stdin=True,
+                        stdout=True, tty=False,
+                        _preload_content=False)
+
+        source_file = input_file_name
+        dest_file = '/root/Desktop/dump.melt'
+        with TemporaryFile() as tar_buffer:
+            with tarfile.open(fileobj=tar_buffer, mode='w') as tar:
+                tar.add(source_file, dest_file)
+
+            tar_buffer.seek(0)
+            commands = []
+            commands.append(tar_buffer.read())
+            while resp_forcp.is_open():
+                resp_forcp.update(timeout=1)
+                if resp_forcp.peek_stdout():
+                    print("STDOUT: %s" % resp_forcp.read_stdout())
+                if resp_forcp.peek_stderr():
+                    print("STDERR: %s" % resp_forcp.read_stderr())
+                if commands:
+                    c = commands.pop(0)
+                    resp_forcp.write_stdin(c.decode())
+                else:
+                    break
+            resp_forcp.close()
+    except ApiException as e:
+        print("Exception when copying files from galaxy-web-*** to ubuntu-desk-vmd-*** %s \n" % e)
+
+def main():
+    #获取tools的.xml配置文件中的输入输出参数
+    input_file_galaxy = sys.argv[1]
+    input_file_name = 'dump.melt'
+    current_path = os.path.abspath('.')
+    input_file_dir_name = os.path.join(current_path, input_file_name)
+    output_file_galaxy = sys.argv[2]
+    output_file_name='log.txt'
+    output_filename_dir_name=os.path.join(current_path,output_file_name)
+
+    with open(input_file_galaxy, 'r') as fread:
+        for line in fread:
+            with open(input_file_dir_name, "a") as fwrite:
+                fwrite.write(line)
+
+    #集群内部即pod内使用，加载config配置文件
+    config.load_incluster_config()
+    namespace='galaxy2'
+    core_v1=client.CoreV1Api()
+    #开始在pod即galaxy-web-***中执行cp文件到ubuntu-desk-vm-***的命令
+    exec_commands(core_v1,namespace,input_file_dir_name)
+
+    cmd_for_log=r'echo "Please to visit http://192.168.108.109:30008 to use vmd for visualizatio! \n">>log.txt'
+    os.system(cmd_for_log)
+    #开始将输出文件写入到output_file_galaxy = sys.argv[2]
+    with open(output_filename_dir_name, 'r') as fread2:
+        for line in fread2:
+            with open(output_file_galaxy, 'a') as fwrite2:
+                fwrite2.write(line)
+
+if __name__ == '__main__':
+    main()
